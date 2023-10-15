6b5315ce913ebf818e0ac942ad97086e9830eef1
Ben Selby <benmatselby@gmail.com>
2023-10-15 15:45:39 +0100

    Parse the phpcs standards output a little better
    Resolves #185

--- tests/test_phpcs.py
+++ tests/test_phpcs.py
@@ -2,15 +2,16 @@ import os
 import re
 import sys
 import sublime
-from unittest import TestCase
 
+from unittest import TestCase
+from unittest.mock import patch
 
 from Phpcs.phpcs import Sniffer
 
 
-class TestDiffViewInternalFunctions(TestCase):
+class TestSniffer(TestCase):
     def test_we_can_build_up_the_correct_executable_string_when_we_prefix(self):
-        php_path = "/bin/php"
+        php_path = "/opt/homebrew/bin/php"
         s = sublime.load_settings("phpcs.sublime-settings")
 
         s.set("phpcs_php_prefix_path", php_path)
@@ -22,9 +23,20 @@ class TestDiffViewInternalFunctions(TestCase):
     def test_we_can_build_up_the_correct_executable_string_when_we_dont_prefix(self):
         s = sublime.load_settings("phpcs.sublime-settings")
 
-        s.set("phpcs_php_prefix_path", "/bin/php")
+        s.set("phpcs_php_prefix_path", "/opt/homebrew/bin/php")
         s.set("phpcs_commands_to_php_prefix", "")
         s.set("phpcs_executable_path", "")
 
         args = Sniffer().get_executable_args()
         self.assertIn("phpcs", args)
+
+    @patch("Phpcs.phpcs.Sniffer.shell_out")
+    def test_we_can_parse_phpcs_standards_output(self, shell_mock):
+        shell_mock.return_value = (
+            "The installed coding standards are One, NeutronStandard, Two and Three"
+        )
+        standards = Sniffer().get_standards_available()
+
+        expected = ["One", "NeutronStandard", "Two", "Three"]
+
+        self.assertEqual(expected, standards)
